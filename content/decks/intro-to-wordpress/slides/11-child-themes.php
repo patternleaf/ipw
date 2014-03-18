ea06bf1b583b48845626c442f56bf2d293b72403
Eric Miller <github@patternleaf.com>
2014-03-17 19:45:52 -0600

    Ooops.

--- content/decks/intro-to-wordpress/slides/11-child-themes.php
+++ content/decks/intro-to-wordpress/slides/11-child-themes.php
@@ -1,19 +1,18 @@
 <div class="row">
 	<h2>Child Themes</h2>
 	<p>Child themes allow you take a theme as a base and override its templates, css, and overall behavior.</p>
-	<div class="col-md-5">
-		<h3>To Make a Child Theme</h3>
-		<ol>
-			<li>Create a new folder in <code>{wp-install-root}/wp-content/themes</code>. Name it something in lowercase and use dashes instead of spaces.</li>
-			<li>In that folder create a file called <code>style.css</code>.</li>
-			<li>
-				Put the code on the right in.
-			</li>
-		</ol>
-		<p>Full instructions can be found <a href="https://codex.wordpress.org/Child_Themes">on the codex</a>.</p>
-	</div>
-	<div class="col-md-6">
-		<div class="pretty-code non-editable" id="wp-child-theme" style="height:300px; width:700px;">/*
+
+	<h3>To Make a Child Theme</h3>
+	<ol>
+		<li>Create a new folder in <code>{wp-install-root}/wp-content/themes</code>. Name it something in lowercase and use dashes instead of spaces.</li>
+		<li>In that folder create a file called <code>style.css</code>.</li>
+		<li>
+			Put the code below in.
+		</li>
+	</ol>
+	<p>Full instructions can be found <a href="https://codex.wordpress.org/Child_Themes">on the codex</a>.</p>
+
+	<div class="pretty-code non-editable" id="wp-child-theme" style="height:300px; width:700px;">/*
 Theme Name: {Your Theme Name}
 Description: A description of your theme.
 Author: {Your Name}
@@ -21,6 +20,6 @@ Template: {folder name of the theme you're overriding}
 */
 
 @import url("../{folder name of the theme you're overriding}/style.css");
-		</div>
 	</div>
+
 </div>
