47e675b6186f2c32ce6e2011dd927150e88e7447
Eric Miller <github@patternleaf.com>
2014-07-14 06:56:06 -0600

    Things occurred. (?)

--- content/sessions/10/exercises/guestbook-mysql/from-class-6/guestbook-app.php
+++ content/sessions/10/exercises/guestbook-mysql/from-class-6/guestbook-app.php
@@ -1,44 +1,57 @@
 <?php
 
+$gConnection = mysqli_connect(
+	'localhost',
+	'username',
+	'password',
+	'my_database'
+);
+
 function guestbookWasSigned() {
 	return array_key_exists('submit', $_POST);
 }
 
 function addEntryToGuestbook() {
+	global $gConnection;
+
 	$name = $_POST['name'];
 	$title = $_POST['title'];
 	$message = $_POST['message'];
-	
-	// load file in
-	$entries = json_decode(file_get_contents('messages.json'));
-	
-	$entries[] = array(
-		'name' => $_POST['name'],
-		'title' => $_POST['title'],
-		'message' => $_POST['message'],
-		'date' => date('F j, Y, g:i a')
-	);
-	
-	file_put_contents('messages.json', json_encode($entries));
+
+	$safe_name = mysqli_real_escape_string($gConnection, $name);
+	$safe_message = mysqli_real_escape_string($gConnection, $message);
+	$safe_title = mysqli_real_escape_string($gConnection, $title);
+
+	$query = 'insert into entries values ("'.$safe_name.'", "'.$safe_title.'", "'.$safe_message.'", NOW());';
+
+	$queryResult = mysqli_query($gConnection, $query);
+
+	error_log(mysqli_error($gConnection));
 }
 
 function printEntries() {
-	
-	$messageData = json_decode(file_get_contents('messages.json'));
-	
-	foreach ($messageData as $messageDatum) {
-		echo '
-			<li class="row">
-				<div class="3u">
-					<h3>'.$messageDatum->title.'</h3>
-					<div class="byline">'.$messageDatum->name.'</div>
-					<div class="dateline">'.$messageDatum->date.'</div>
-				</div>
-				<div class="9u">
-					'.$messageDatum->message.'
-				</div>
-			</li>
-	'	;
+	global $gConnection;
+
+	$query = 'select * from entries;';
+
+	$queryResult = mysqli_query($gConnection, $query);
+
+	if ($queryResult) {
+		while ($message = mysqli_fetch_assoc($queryResult)) {
+			echo '
+				<li class="row">
+					<div class="3u">
+						<h3>'.$message['title'].'</h3>
+						<div class="byline">'.$message['name'].'</div>
+						<div class="dateline">'.$message['timestamp'].'</div>
+					</div>
+					<div class="9u">
+						'.$message['message'].'
+					</div>
+				</li>
+		'	;
+		}
 	}
+
 
 }
