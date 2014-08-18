4b692fe92e68dfcbe141c1f53a39344747e53a74
Eric Miller <github@patternleaf.com>
2014-08-18 17:58:10 -0600

    Session 9 stuff. Such as it is.

--- content/sessions/9/session-9.php
+++ content/sessions/9/session-9.php
@@ -28,8 +28,7 @@ ob_start();
 					<div class="col-md-8">
 						<h1>Review</h1>
 						<ul>
-							<li>Review personal projects.</li>
-							<li>Review WordPress themes, jQuery accordion plugin.</li>
+							<li>MySQL-driven guestbook</li>
 						</ul>
 					</div>
 				</section>
@@ -37,43 +36,20 @@ ob_start();
 					<div class="row">
 						<div class="col-md-8">
 							<h1>Lecture</h1>
-							<p>We have a few advanced topics we can talk about:</p>
+							<p>We have a couple of advanced topics we can talk about:</p>
 							<ul>
-								<li>AJAX (AHAH, AJAJ)</li>
-								<li>AJAX with jQuery</li>
-								<li>Closures.</li>
+								<li>RESTful interfaces</li>
 								<li>Pass by reference vs pass by value.</li>
 							</ul>
+							<p>
+								Slides, etc, forthcoming!
+							</p>
 						</div>
 						<div class="col-md-4">
 
 						</div>
 					</div>
-					<div class="row" style="clear:both;">
-						<div class="col-md-10">
-							<h3>AJAX</h3>
 
-							<p>
-								Below are two files demonstrating a very basic ajax request.
-							</p>
-
-							<h4 style="margin-top:2em;"><a href="exercises/ajax-client.php">Client-side</a>.</h4>
-
-							<div class="pretty-code non-editable" id="ajax-client" data-language="php" style="width:100%; height:300px"><?php app()->escapedFileContents(__DIR__.'/exercises/ajax-client.php'); ?></div>
-
-							<h4 style="margin-top:2em;"><a href="exercises/ajax-client.php">Server-side</a>.</h4>
-							<div class="pretty-code non-editable" id="ajax-server" data-language="php" style="width:100%; height:300px"><?php app()->escapedFileContents(__DIR__.'/exercises/ajax-server.php'); ?></div>
-
-							<h4 style="margin-top:2em;">Client-side (with functions broken out).</h4>
-
-							<div class="pretty-code non-editable" id="ajax-client-broken-out" data-language="php" style="width:100%; height:300px"><?php app()->escapedFileContents(__DIR__.'/exercises/ajax-client-broken-out.php'); ?></div>
-
-							<h4>Exercise</h4>
-							<p>
-								Add a "number of paragraphs" option to the form, pass that value to the server, and have the server respond appropriately.
-							</p>
-						</div>
-					</div>
 				</section>
 <!--
 				<section class="section row" id="materials">
@@ -92,8 +68,7 @@ ob_start();
 					<h1>Goals</h1>
 					<div class="col-md-9">
 						<ul>
-							<li>Understand AJAX: what is the difference between an AJAX request and a "regular" HTTP request?</li>
-							<li>Closures in JavaScript.</li>
+							<li>RESTful interfaces and APIs</li>
 							<li>Pass by value versus pass by reference.</li>
 						</ul>
 					</div>
